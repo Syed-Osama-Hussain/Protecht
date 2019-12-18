b4bb18323247f36c2188a643c0882f876f4b965f
Syed Osama Hussain <osamahussain38@gmail.com>
2019-12-18 20:32:33 +0500

    Updates

---
+++ precache-manifest.c8e5c59043357774ed051e0d7f72d5ec.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8989d4089546fa26a3946e02531eef00",
+    "url": "/Protecht/index.html"
+  },
+  {
+    "revision": "6b0c65d43b87787dac63",
+    "url": "/Protecht/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "bbe32f84e5938a599d6f",
+    "url": "/Protecht/static/css/main.0e249986.chunk.css"
+  },
+  {
+    "revision": "6b0c65d43b87787dac63",
+    "url": "/Protecht/static/js/2.aeee9366.chunk.js"
+  },
+  {
+    "revision": "bbe32f84e5938a599d6f",
+    "url": "/Protecht/static/js/main.a77d5147.chunk.js"
+  },
+  {
+    "revision": "128b3135600ed3c78415",
+    "url": "/Protecht/static/js/runtime~main.ec4829f9.js"
+  },
+  {
+    "revision": "8723d7a22e74ddcacf06adda017b52ad",
+    "url": "/Protecht/static/media/about.8723d7a2.jpg"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/Protecht/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/Protecht/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/Protecht/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/Protecht/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "c1b6c68a7134ecfea21c0bc54e1d4532",
+    "url": "/Protecht/static/media/fam.c1b6c68a.jpg"
+  },
+  {
+    "revision": "a76c7b4ce725bb07b1572699a5a78c62",
+    "url": "/Protecht/static/media/hero-image-1.a76c7b4c.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/Protecht/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/Protecht/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/Protecht/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/Protecht/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "63499a39682b11390ebe625b305a8e17",
+    "url": "/Protecht/static/media/one.63499a39.jpg"
+  },
+  {
+    "revision": "3842becac59acca82a5189bf7eeac82f",
+    "url": "/Protecht/static/media/register-bg.3842beca.png"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/Protecht/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "3c534f7c85f82009f9907e03782230be",
+    "url": "/Protecht/static/media/three.3c534f7c.jpg"
+  },
+  {
+    "revision": "e24d9ffbc37ba5eb82b8f6e47c208dcf",
+    "url": "/Protecht/static/media/two.e24d9ffb.jpg"
+  }
+]);
